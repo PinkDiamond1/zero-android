910d25dd104c5b11c003ed5fab282e58f88cc838
Shahroz <shrozali@outlook.com>
2022-09-16 03:10:22 +0500

    Text clear bug fixed

--- feature-messages/src/main/java/com/zero/android/feature/messages/ui/components/UserInputPanel.kt
+++ feature-messages/src/main/java/com/zero/android/feature/messages/ui/components/UserInputPanel.kt
@@ -2,7 +2,12 @@ package com.zero.android.feature.messages.ui.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -10,8 +15,13 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -121,9 +131,8 @@ fun UserInputPanel(
 			onMessageSent = {
 				if (it.isNotEmpty()) {
 					onMessageSent(it)
-					textState = TextFieldValue()
-					keyboardController?.hide()
 				}
+				textState = TextFieldValue()
 			}
 		)
 		Icon(
